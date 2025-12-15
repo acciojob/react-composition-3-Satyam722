2560e424dd9cffe48337ab575717608623454778
Satyam <nikamsatyam187@gmail.com>
2025-12-15 13:53:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./ToolTip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tooltip text="This is tool tip">
+        <h2>Hover Over Me</h2> 
+        </Tooltip>
+
+      <br /> <br />
+
+        <Tooltip text="This is another tooltip">
+          <h3>Hover over me to see another tooltip.</h3>
+        </Tooltip>
+
     </div>
   )
 }
